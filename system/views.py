2df75596bb2f8bfc59d326135351e9091675a42c
zzq <13041129187@163.com>
2021-11-25 17:42:38 +0800

    add html freetable

--- system/views.py
+++ system/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import sqlite3
 
 # Create your views here.
+
 
 def main(request):
 
@@ -21,10 +23,15 @@ def configed(request):
     return render(request, 'system/configed.html',)
 
 def free(request):
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()
 
     if request.method == 'POST':
         pass
 
     else:
-        pass
-    return render(request, 'system/free.html',)
+        sql = " SELECT dev_ID,description FROM input_select WHERE free = 1"
+        cursor.execute(sql)
+        form_free = cursor.fetchall()
+
+    return render(request, 'system/free.html',{'form':form_free})
