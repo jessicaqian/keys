0239e362e9656490997f9629adf1e785bf314352
zzq <13041129187@163.com>
2021-12-02 17:39:19 +0800

    update 12.2

--- system/views.py
+++ system/views.py
@@ -1,8 +1,11 @@
+# coding=utf-8
 from django.shortcuts import render
 import sqlite3
+import json
 from .forms import ConfigForm
 
 # Create your views here.
+
 
 
 def main(request):
@@ -39,11 +42,28 @@ def free(request):
     return render(request, 'system/free.html',{'form':form_free})
 
 def config(request):
-    conn = sqlite3.connect('db.sqlite3')
-    cursor = conn.cursor()
 
     if request.method == 'POST':
-        return render(request, 'system/config.html',)
+        form = ConfigForm(request.POST)
+        if form.is_valid():
+            info_dict = form.cleaned_data
+            conn = sqlite3.connect('db.sqlite3')
+            dict_keyname = {'1':info_dict['key1'],'2':info_dict['key2'],'3':info_dict['key3'],'4':info_dict['key4'],
+                            '5':info_dict['key5'],'6':info_dict['key6'],'7':info_dict['key7'],'8':info_dict['key8'],
+                            '9':info_dict['key9'],'10':info_dict['key10'],'11':info_dict['key11'],'12':info_dict['key12']}
+            cursor = conn.cursor()
+            sql = "INSERT INTO keys_set (inputID,inputName,ip,description,keyName) values('"+info_dict['id']+"','"+info_dict['name']+"','"+info_dict['ip']+"','"+info_dict['description']+"','"+json.dumps(dict_keyname,ensure_ascii=False)+"')"
+
+            cursor.execute(sql)
+            conn.commit()
+            sql = " UPDATE input_select SET free = 0 WHERE dev_ID = '"+info_dict['id']+"'"
+            cursor.execute(sql)
+            conn.commit()
+            conn.close()
+
+
+
+        return render(request, 'system/keyconfig.html',{'dict':info_dict})
 
     else:
         form = ConfigForm()
